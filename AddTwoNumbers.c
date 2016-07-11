ca708905de54dbb742336e84750bf1f4b0c35efd
Kevin Zhang <kevin1.zhang@intel.com>
2016-07-11 14:52:14 -0500

    Simplify a bit

--- AddTwoNumbers.c
+++ AddTwoNumbers.c
@@ -18,21 +18,26 @@
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     int temp, carry = 0;
     struct ListNode *NodeCurrent, *NodeHead = (struct ListNode *)NULL, *NodeTail;
-    struct ListNode *l1Current = l1, *l2Current = l2;
-    struct ListNode NullNode;
-    NullNode.val = 0;
-    NullNode.next = &NullNode;
+    struct ListNode *p = l1, *q = l2;
     
     if (l1 == NULL && l2 == NULL)
         return NULL;
     
     do {
-        NodeCurrent = (struct ListNode *) malloc (sizeof(struct ListNode));
-        temp = l1Current->val + l2Current->val + carry;
+        temp = carry;
+        if (p != NULL) {
+            temp += p->val;
+            p = p->next;
+        }
+        if (q != NULL) {
+            temp += q->val;    
+            q = q->next;
+        }
+//      printf("temp is: %d\n", temp);
+        NodeCurrent = (struct ListNode *) calloc (1, sizeof(struct ListNode));
         NodeCurrent->val = temp >= 10 ? (temp - 10): temp;
-        carry = temp >= 10 ? 1:0;
+        carry = (temp >= 10) ? 1:0;
 
-        NodeCurrent->next = NULL;
         if (NodeHead == NULL) { //first node
             NodeHead = NodeCurrent;
             NodeTail = NodeCurrent;
@@ -40,15 +45,7 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
             NodeTail->next = NodeCurrent;
             NodeTail = NodeCurrent;
         }
-//        printf("current val=%d\n", NodeCurrent->val);
-
-        l1Current = l1Current->next;
-        l2Current = l2Current->next;
-        if (l1Current == NULL)
-            l1Current = &NullNode;
-        if (l2Current == NULL)
-            l2Current = &NullNode;
-    } while (l1Current != &NullNode || l2Current != &NullNode || carry != 0);
+    } while (p != NULL || q != NULL || carry != 0);
 
     return NodeHead;
 }
